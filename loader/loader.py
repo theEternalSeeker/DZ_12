5fec197c1e389522520ad47e5514f6f2de4f0674
CocoJambo <igortrue@mail.ru>
2023-12-10 00:25:56 +0300

    реализован ШАГ 5

---
+++ loader/loader.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, render_template
+from functions import load_posts, upload_posts
+
+loader_blueprint = Blueprint('loader', __name__, url_prefix="/post", static_folder="static", template_folder='templates')
+@loader_blueprint.route('/form/')
+def form():
+    return render_template('post_form.html')
+
+@loader_blueprint.route('/upload/', methods=["POST"])
+def upload():
+    try:
+        file = request.files['picture']
+        filename = file.filename
+        content = request.values['content']
+        posts = load_posts()
+        posts.append({
+            'pic': f'/uploads/images/{filename}',
+            'content': content
+
+        })
+        upload_posts(posts)
+        file.save(f'uploads/images/{filename}')
+
+    except FileNotFoundError:
+        return '<h1> Файл не найден!</h1>'
+    else:
+        return render_template('post_uploaded.html', pic=f'/uploads/images/{filename}', content=content)
+
+#
