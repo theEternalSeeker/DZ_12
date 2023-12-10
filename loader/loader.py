3c025eea8816de4d59c4b3aad9243d9eb710a3b5
CocoJambo <igortrue@mail.ru>
2023-12-10 16:03:46 +0300

    ШАГ 6 добавлено логирование

--- loader/loader.py
+++ loader/loader.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, render_template
 from functions import load_posts, upload_posts
+import logging
+
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
 
 loader_blueprint = Blueprint('loader', __name__, url_prefix="/post", static_folder="static", template_folder='templates')
 @loader_blueprint.route('/form/')
@@ -20,9 +23,12 @@ def upload():
         })
         upload_posts(posts)
         file.save(f'uploads/images/{filename}')
+        if filename.split('.')[-1] not in ["png", "jpg", "jpeg"]:
+            logging.info("Файл не является изображением!")
         return render_template('post_uploaded.html', pic=f'/uploads/images/{filename}', content=content)
 
     except FileNotFoundError:
+        logging.error("Ошибка при загрузке файла!")
         return '<h1> Файл не найден!</h1>'
     # else:
     #     return render_template('post_uploaded.html', pic=f'/uploads/images/{filename}', content=content)
--- main/main.py
+++ main/main.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, render_template
 from functions import load_posts
+import logging
+
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 @main_blueprint.route('/')
@@ -9,6 +12,7 @@ def main():
 @main_blueprint.route('/search/')
 def search():
     search_by = request.args['s']
+    logging.info(f"Слово для поиска : {search_by} ")
     posts = [x for x in load_posts() if search_by.lower() in x['content'].lower()]
     return render_template('post_list.html', search_by=search_by, posts=posts)
 
